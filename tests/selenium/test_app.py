135269c1791fb215ea1ed02420225711ac040fd9
Dustin Van Buskirk <dev@vanbuskirk.me>
2018-04-23 22:39:05 -0700

    updating tests

--- tests/selenium/test_app.py
+++ tests/selenium/test_app.py
@@ -3,6 +3,8 @@ import time
 import os
 
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
 
 url = os.getenv('URL')
 
@@ -18,18 +20,25 @@ class VoteTest(unittest.TestCase):
   def confirm_a(self):
     browser = self.browser
     browser.get(url)
-    vote_box_a = browser.find_element_by_name('a')
-    assert vote_box_a.text == 'a'
+    self.assertTrue(self.is_element_present(By.NAME,"a"))
 
   def confirm_b(self):
     browser = self.browser
     browser.get(url)
-    vote_box_a = browser.find_element_by_name('a')
-    assert vote_box_a.text == 'a'
+    self.assertTrue(self.is_element_present(By.NAME,"b"))
 
   def tear_down(self):
     self.browser.quit()
 
+  def is_element_present(self, how, what):
+    """
+    Helper method to confirm the presence of an element on page
+    :params how: By locator type
+    :params what: locator value
+    """
+    try: self.browser.find_element(by=how, value=what)
+    except NoSuchElementException: return False
+    return True
 
 if __name__ == '__main__':
     unittest.main()
