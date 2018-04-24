0a02f75c9ce33ee7236fb0ffe5de7aa005362b5e
Dustin Van Buskirk <dev@vanbuskirk.me>
2018-04-23 23:02:38 -0700

    rolling back updates

--- tests/selenium/test_app.py
+++ tests/selenium/test_app.py
@@ -9,21 +9,24 @@ from selenium.webdriver.common.by import By
 url = os.getenv('URL')
 
 class VoteTest(unittest.TestCase):
-  @classmethod
-  def setUpClass(inst):
-      # create a new Chrome session
-      inst.driver = webdriver.Chrome()
-      inst.driver.implicitly_wait(10)
-      inst.driver.maximize_window()
-      # navigate to the application home page
-      inst.driver.get(url)
-      inst.driver.title
+
+  def setup(self):
+    caps = {'browserName': os.getenv('BROWSER', 'chrome')}
+    self.browser = webdriver.Remote(
+      command_executor='http://selenium_hub:4444/wd/hub',
+      desired_capabilities=caps
+    )
 
   def test_confirm_a(self):
+    self.browser.get(url)
     self.assertTrue(self.is_element_present(By.NAME,"a"))
 
   def test_confirm_b(self):
+    self.browser.get(url)
     self.assertTrue(self.is_element_present(By.NAME,"b"))
+
+  def tear_down(self):
+    self.browser.quit()
 
   def is_element_present(self, how, what):
     """
@@ -31,14 +34,9 @@ class VoteTest(unittest.TestCase):
     :params how: By locator type
     :params what: locator value
     """
-    try: self.driver.find_element(by=how, value=what)
+    try: self.browser.find_element(by=how, value=what)
     except NoSuchElementException: return False
     return True
-
-  @classmethod
-  def tearDownClass(inst):
-      # close the browser window
-      inst.driver.quit()
 
 if __name__ == '__main__':
     unittest.main()
