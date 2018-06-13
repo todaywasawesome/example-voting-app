1b804323d290c4eafec10585f1c270d80ee01e1e
Dan <tdangarfield@gmail.com>
2018-06-13 09:36:18 -0700

    Vader vs Kylo

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Warriors")
-option_b = os.getenv('OPTION_B', "Cavs")
+option_a = os.getenv('OPTION_A', "Vader")
+option_b = os.getenv('OPTION_B', "Kylo")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
