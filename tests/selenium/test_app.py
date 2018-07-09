0bb4b91e48e9b8b8a28a10fdf19a8c30b31e89d3
Dan <tdangarfield@gmail.com>
2018-07-09 12:52:28 -0700

    Try removing useless stuff

--- tests/selenium/test_app.py
+++ tests/selenium/test_app.py
@@ -28,9 +28,9 @@ class VoteTest(unittest.TestCase):
   def tearDown(self):
     with open('test1.txt', 'a') as test_results:
       test_results.write("https://saucelabs.com/beta/tests/" + self.browser.session_id+ ",")
-      jobId = self.webdriver.session_id
-      hmac.new(sl_username+ ":" + sl_access_key, jobId, md5).hexdigest()
-      test_results.write(" https://saucelabs.com/beta/builds/" + jobId)
+      # jobId = self.webdriver.session_id
+      # hmac.new(sl_username+ ":" + sl_access_key, jobId, md5).hexdigest()
+      # test_results.write(" https://saucelabs.com/beta/builds/" + jobId)
 
   def test_confirm_title(self):
     browser = self.browser
